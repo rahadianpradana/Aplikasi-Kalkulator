1c6b1ceeffe6db40e25909bb2aa544fb3de1232a
rahadianpradana <62988440+rahadianpradana@users.noreply.github.com>
2020-04-15 21:48:37 +0700

    Add files via upload

---
+++ Kalkulator/src/kalkulators/kalkulator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulators;
+
+/**
+
+ */
+public class kalkulator { //Class main
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new kalkulators.HomeKalkulator().setVisible(true); //Memanggil file HomeKalkulator.java
+    }
+}
